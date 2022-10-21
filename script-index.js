ec1a8f4aec50bd331d6107d01600d17b27662b88
Wallace Rangel <wallacerangelgama7@gmail.com>
2022-10-21 10:52:18 -0300

    To-Do-List-v1.5

--- script-index.js
+++ script-index.js
@@ -24,3 +24,39 @@ function nomeValidar(){
         console.log("Nome Aceito");
     }
 }
+
+function loginValidar(){
+    if(login.value.length > 10)
+    {
+        console.log("Nome menor que 10 caracteres");
+    }
+    else
+    {
+        console.log("Nome Aceito");
+    }
+}
+
+function emailValidar(){
+    if(email.value.length > 10)
+    {
+        console.log("Tem @");
+    }
+    else
+    {
+        console.log("Não tem @");
+    }
+}
+function compareSenha(){
+    if(senha.value == "" && confsenha.value == "" )
+    {
+        console.log("Campo obrigatorio")
+    }
+    else if(senha.value == confsenha.value )
+    {
+        console.log("Senhas Batem");
+    }
+    else
+    {
+        console.log("Senhas não batem");
+    }
+}
